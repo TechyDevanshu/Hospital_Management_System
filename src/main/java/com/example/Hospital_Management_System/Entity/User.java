0b494fc5d0ee0bb1a99b71b6e4517109611e343b
Devanshu Urmaliya <devanshuurmaliya@gmail.com>
2025-09-27 15:16:38 +0530

    Adding Login functionalities and make some UI changes.

---
+++ src/main/java/com/example/Hospital_Management_System/Entity/User.java
@@ -0,0 +1,30 @@
+package com.example.Hospital_Management_System.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/example/Hospital_Management_System/Service/UserService.java
@@ -0,0 +1,25 @@
+package com.example.Hospital_Management_System.Service;
+
+import com.example.Hospital_Management_System.Entity.User;
+import com.example.Hospital_Management_System.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public boolean register(User user) {
+        if (userRepository.findByUsername(user.getUsername()) != null) {
+            return false;
+        }
+        userRepository.save(user);
+        return true;
+    }
+
+    public boolean login(String username, String password) {
+        User user = userRepository.findByUsername(username);
+        return user != null && user.getPassword().equals(password);
+    }
+}
